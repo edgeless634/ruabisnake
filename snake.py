4e8593052848ff638e355d3e8bab4ad16ea0349e
edgeless634 <edgeless634@protonmail.com>
2020-01-28 00:11:05 +0800

    Revert "无限模式"
    This reverts commit 358a298d8e94c499ccf0251f372ffdcad119b6d5.
    
    优化炸了,回退版本

--- snake.py
+++ snake.py
@@ -15,9 +15,6 @@ showMpTime=0 #地图刷新的最短时间
 clearCommand="cls" if os.name=="nt" else "clear"
 
 def mptotype(i,j):
-    '''
-    把地图转换成相应的代号
-    '''
     if (i,j) in snake:
         return 1 if snake.index((i,j))==0 else 2 #snake's head jor body
     elif i==applei and j==applej:
@@ -28,9 +25,6 @@ def mptotype(i,j):
         return 4 #air
 
 def printmp():
-    '''
-    打印地图
-    '''
     os.system(clearCommand)
     print(mpchar[0]*(mplength+2))
     for i in range(1,mplength+1):
@@ -41,18 +35,11 @@ def printmp():
     print(mpchar[0]*(mplength+2))
 
 def stallForTime():
-    '''
-    在够不到果子的时候苟命
-    基本思路:
-    1.找到头部能够得到,且离尾部最近的身体
-    2.计算那段身子到周围几个空位的距离
-    3.往离那段身子最远的空位走
-    '''
     queue=[(0,0),snake[0]]
     queh=0
     target=(0,0)
     mxtarget=0
-    while queh<len(queue)-1:#找到头部能够得到,且离尾部最近的身体
+    while queh<len(queue)-1:
         queh+=1
         for i in range(0,4):
             nxti,nxtj=queue[queh][0]+di[i],queue[queh][1]+dj[i]
@@ -67,7 +54,7 @@ def stallForTime():
     dict={target:0}
     queue=[(0,0),target]
     queh=0
-    while queh<len(queue)-1:#计算那段身子到周围几个空位的距离
+    while queh<len(queue)-1:
         queh+=1
         for i in range(0,4):
             nxti,nxtj=queue[queh][0]+di[i],queue[queh][1]+dj[i]
@@ -76,7 +63,7 @@ def stallForTime():
             queue.append((nxti,nxtj))
             dict[queue[-1]]=dict[queue[queh]]+1 
     k,kmax=-1,-1
-    for i in range(0,4):#往离那段身子最远的空位走
+    for i in range(0,4):
         nxti,nxtj=snake[0][0]+di[i],snake[0][1]+dj[i]
         if min(nxti,nxtj)<1 or nxti>mplength or nxtj>mpwidth or mptotype(nxti,nxtj)<3:
             continue
@@ -87,15 +74,12 @@ def stallForTime():
     return k
 
 def autoChangeDirection():
-    '''
-    返回通向果子的路线的生成器
-    '''
     queue=[(0,0),snake[0]]
     fa=[0,0]
     lastdire=[-1,-1]
     target=(applei,applej)
     queh=0
-    while queh<len(queue)-1:#搜索
+    while queh<len(queue)-1:
         queh+=1
 #        print(f"正在搜索{queue[queh]} 目标是{target[0]},{target[1]}")
         for i in range(0,4):
@@ -111,7 +95,7 @@ def autoChangeDirection():
             break
     else:
         return
-    stack=[]#回溯
+    stack=[]
     p=len(queue)-1
     while p>1:
         stack.append(lastdire[p])
@@ -122,9 +106,6 @@ def autoChangeDirection():
 """
 direlist=[]
 def changeDirection():
-    '''
-    输入并返回蛇下一步的方向(完全版)
-    '''
     global direlist
     g=autoChangeDirection()
     direlist=[i for i in g]
@@ -137,9 +118,6 @@ def changeDirection():
 """
 direlist=[]
 def changeDirection():
-    '''
-    输入并返回蛇下一步的方向
-    '''
     global direlist
     if len(direlist) <= 1:
         g=autoChangeDirection()
@@ -152,18 +130,14 @@ def changeDirection():
     return direlist[0]
 
 def putApple():
-    '''
-    更新苹果的位置
-    '''
     global applei,applej
     applei,applej=random.randint(1,mplength),random.randint(1,mpwidth)
     while (applei,applej) in snake:
         applei,applej=random.randint(1,mplength),random.randint(1,mpwidth)
 
+
+    
 def startGame():
-    '''
-    开始一局游戏
-    '''
     printmp()
     while True:
         tstart=time.time()
@@ -173,7 +147,7 @@ def startGame():
         if mptotype(nxti,nxtj)<3 and snake[-1]!=(nxti,nxtj):
             print(f"""
 GAME OVER
-The length of the snake is {len(snake)} 
+The length of snake is {len(snake)} 
             """)
             return
         if nxti==applei and nxtj==applej:
@@ -184,24 +158,17 @@ The length of the snake is {len(snake)}
         printmp()
         time.sleep(showMpTime+time.time()-tstart)
 
-def infinityMode():
-    '''
-    无限模式
-    - 无限自动开始游戏
-    - 记录已进行的游戏局数和蛇的总长度
-    '''
-    global snake,dire,applei,applej
-    gameCount=0
-    totalLength=0
-    while True:
-        startGame()
-        totalLength+=len(snake)
-        gameCount+=1
-        print(f"已经进行了{gameCount}局,平均长度是{totalLength/gameCount}")
-        snake=[(3,1),(2,1),(1,1)]
-        dire=1
-        applei,applej=4,4
-        time.sleep(3)
-
 #startGame()
-infinityMode()
+
+
+gameCount=0
+totalLength=0
+while True:
+    startGame()
+    totalLength+=len(snake)
+    gameCount+=1
+    print(f"已经进行了{gameCount}局,平均长度是{totalLength/gameCount}")
+    snake=[(3,1),(2,1),(1,1)]
+    dire=1
+    applei,applej=4,4
+    time.sleep(3)
